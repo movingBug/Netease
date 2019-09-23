c79bf65d2e685960cff5b1d8c264979365a6d34f
wang <839584942@qq.com>
2019-09-23 15:53:31 +0800

    首页排版

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,34 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2019-09-23 15:02:34
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2019-09-23 15:48:23
+ -->
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="loginBox">
+    <div class="logo">
+      <img src="http://yanxuan.nosdn.127.net/bd139d2c42205f749cd4ab78fa3d6c60.png" alt />
+    </div>
+    <div class="loginMain">
+      <p class="userinput">
+        <input type="text" />
+      </p>
+      <p class="passwordInput">
+        <input type="text" />
+      </p>
+      <div class="loginbtn">
+        <input type="button" value="登录" />
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+
+};
+</script>
+<style lang="scss">
+@import url("./index.scss");
+</style>
